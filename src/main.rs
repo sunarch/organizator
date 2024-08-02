e690af7c5fc4a0b9938f1bd09584f98ae837fc65
András Németh <sunarch@protonmail.com>
2024-08-03 00:14:42 +0200

    Add config dir loading

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+use std::fs;
+use std::path::Path;
+use directories_next::ProjectDirs;
+
+const DIRS_QUALIFIER: &str = "dev";
+const DIRS_ORGANIZATION: &str = "sunarch";
+
+
 fn main() {
     let option_version_short: String = String::from("-v");
     let option_version_long: String = String::from("--version");
@@ -19,5 +27,20 @@ fn main() {
 
         },
         None => {},
+    }
+
+    let project_dirs: ProjectDirs = match ProjectDirs::from(DIRS_QUALIFIER, DIRS_ORGANIZATION,  env!("CARGO_PKG_NAME")) {
+        Some(value) => { value }
+        None => {
+            panic!("Unable to load project directory paths!")
+        }
+    };
+
+    let config_dir: &Path = project_dirs.config_dir();
+    match fs::create_dir_all(config_dir) {
+        Ok(_) => {}
+        Err(_) => {
+            panic!("Unable to load create project config directory!")
+        }
     }
 }
