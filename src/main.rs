1d4c4b1fb22364aeefb4bdce6a26c6189695e7a8
András Németh <sunarch@protonmail.com>
2024-08-04 02:03:40 +0200

    Start TUI mode with Ratatui as a dependency

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,15 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 mod config;
+mod tui;
 
 
 fn main() {
     let option_version_short: String = String::from("-v");
     let option_version_long: String = String::from("--version");
+
+    let option_tui: String = String::from("--tui");
+    let mut run_tui: bool = false;
 
     if let Some(argument) = std::env::args().nth(1) {
         if argument == option_version_short || argument == option_version_long {
@@ -15,10 +19,18 @@ fn main() {
             let version = env!("CARGO_PKG_VERSION");
             println!("{name} {version}");
             return;
-        } else {
+        }
+        else if argument == option_tui {
+            run_tui = true;
+        }
+        else {
             panic!("Unrecognized option: {argument}");
         }
     }
 
     (_, _, _) = config::load_data_dirs();
+
+    if run_tui {
+        tui::run().expect("Error running TUI");
+    }
 }
---
+++ src/tui.rs
@@ -0,0 +1,52 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+use std::io;
+use std::time::Duration;
+
+use ratatui::{
+    backend::CrosstermBackend,
+    crossterm::{
+        event::{self, KeyCode, KeyEventKind},
+        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+        ExecutableCommand,
+    },
+    style::Stylize,
+    widgets::Paragraph,
+    Terminal,
+};
+
+
+pub(crate) fn run() -> io::Result<()> {
+    io::stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(io::stdout()))?;
+    terminal.clear()?;
+
+    let program_name: &str = env!("CARGO_PKG_NAME");
+    loop {
+        terminal.draw(|frame| {
+            let area = frame.size();
+            frame.render_widget(
+
+                Paragraph::new(format!("{program_name} (press 'q' to quit)"))
+                    .light_green()
+                    .on_black(),
+                area,
+            );
+        })?;
+
+        if event::poll(Duration::from_millis(16))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
+
+    io::stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    return Ok(());
+}
