f9f7625968f8c0a347e5e103da7f8e2f25e51c52
András Németh <sunarch@protonmail.com>
2024-08-31 00:06:03 +0200

    Move progressive task to next day if done for today

--- src/tasks/type_progressive.rs
+++ src/tasks/type_progressive.rs
@@ -4,11 +4,12 @@
 
 use std::path::Path;
 // dependencies
-use chrono::{DateTime, Datelike, Local, NaiveDate};
+use chrono::NaiveDate;
 use serde;
 // internal
 use crate::tasks::task::Task;
 use crate::tasks::types;
+use crate::time;
 
 pub(crate) const DIR_NAME: &str = "progressive";
 
@@ -26,8 +27,6 @@ struct Data {
 }
 
 pub(crate) fn parse(file_path: &Path) -> Option<(NaiveDate, Task)> {
-    let timestamp: DateTime<Local> = Local::now();
-
     let data: Data = match types::load(file_path) {
         None => {
             return None;
@@ -35,17 +34,36 @@ pub(crate) fn parse(file_path: &Path) -> Option<(NaiveDate, Task)> {
         Some(data) => data,
     };
 
+    let mut last_date_string: String = Default::default();
     let mut task_note: String = String::new();
     for item in data.items {
         if item.done.is_empty() {
             task_note = item.title;
             break;
+        } else {
+            last_date_string = item.done;
         }
     }
 
+    let last_date = match NaiveDate::parse_from_str(last_date_string.as_str(), "%Y-%m-%d") {
+        Err(_) => {
+            println!(
+                "Failed to convert last date in progressive task: '{}' ({})",
+                last_date_string, data.title
+            );
+            return None;
+        }
+        Ok(date) => date,
+    };
+
+    let today: NaiveDate = time::today();
+    let mut task_date: NaiveDate = today;
+    if last_date == today {
+        task_date = time::add_days(task_date, 1).expect("Failed to add day.");
+    };
+
     return Some((
-        NaiveDate::from_ymd_opt(timestamp.year(), timestamp.month(), timestamp.day())
-            .expect("Failed to create NaiveDate"),
+        task_date,
         Task {
             frequency: String::from("(PR)"),
             title: data.title,
